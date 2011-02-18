38094da5c6f0f2260ce133f66e71eee5c51196f1
Diego López León <dieguitoll@gmail.com>
2011-02-17 22:57:49 -0300

    changing output format

--- src/main/java/org/plugtree/drools/shell/outputbuilders/KnowledgePackageCollectionOutputBuilder.java
+++ src/main/java/org/plugtree/drools/shell/outputbuilders/KnowledgePackageCollectionOutputBuilder.java
@@ -8,12 +8,14 @@ import java.util.Collection;
  * creation date: 2/17/11
  */
 public class KnowledgePackageCollectionOutputBuilder implements OutputBuilder<Collection<KnowledgePackage>> {
+
     @Override
     public String getOutput(Collection<KnowledgePackage> input) {
         StringBuilder output = new StringBuilder();
         for(KnowledgePackage kpackage : input){
+            output.append("* ");
             output.append(kpackage.getName());
-            output.append(", ");
+            output.append(CR);
         }
         if (output.length() > 0) {
             output.delete(output.length() - 2, output.length());
