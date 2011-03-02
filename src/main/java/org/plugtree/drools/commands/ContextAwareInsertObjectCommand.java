de473a938c7754ac18dceb4ba3dbea21585621a9
Diego López León <dieguitoll@gmail.com>
2011-03-02 10:53:03 -0300

    create insertFactCommand for each execute invocation

--- src/main/java/org/plugtree/drools/commands/ContextAwareInsertObjectCommand.java
+++ src/main/java/org/plugtree/drools/commands/ContextAwareInsertObjectCommand.java
@@ -7,7 +7,6 @@ import org.drools.command.runtime.rule.InsertObjectCommand;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.rule.FactHandle;
 import org.plugtree.drools.ext.ObjectStore;
-import org.plugtree.drools.shell.outputbuilders.InsertedObjectResult;
 
 /**
  * creation date: 3/1/11
@@ -15,23 +14,24 @@ import org.plugtree.drools.shell.outputbuilders.InsertedObjectResult;
 public class ContextAwareInsertObjectCommand implements GenericCommand<ObjectStoreOperationResult> {
 
     private ObjectStore objectStore;
-    private InsertObjectCommand insertObjectCommand;
+    private Object objectToInsert;
 
     public ContextAwareInsertObjectCommand(ObjectStore objectStore) {
         this.objectStore = objectStore;
-        this.insertObjectCommand = new InsertObjectCommand();
     }
 
     @Override
     public ObjectStoreOperationResult execute(Context context) {
-        final FactHandle handle = insertObjectCommand.execute(context);
+        if( objectToInsert == null )
+            throw new IllegalStateException("You must set object to insert first");
+        final FactHandle handle = new InsertObjectCommand(objectToInsert).execute(context);
         StatefulKnowledgeSession ksession = ((KnowledgeCommandContext) context).getStatefulKnowledgesession();
         final Object insertedObject = ksession.getObject(handle);
         final int assignedId = objectStore.put(insertedObject);
         return new ObjectStoreOperationResult(assignedId, insertedObject);
     }
 
-    public void setObject(Object object) {
-        insertObjectCommand.setObject(object);
+    public void setObject(Object outputObject) {
+        this.objectToInsert = outputObject;
     }
 }
